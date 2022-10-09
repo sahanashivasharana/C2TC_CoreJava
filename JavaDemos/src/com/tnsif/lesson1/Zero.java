cd4f063b61b94be025d9c4587b3c9a99c036082c
asus <asus@LAPTOP-R7BD4ODT>
2022-10-09 23:45:17 +0530

    first commit

--- JavaDemos/src/com/tnsif/lesson1/Zero.java
+++ JavaDemos/src/com/tnsif/lesson1/Zero.java
@@ -1,11 +1,10 @@
-//program to display output on console
 package com.tnsif.lesson1;
 
 public class Zero {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+System.out.println("Hello World");
 	}
 
 }
